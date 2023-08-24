d7541cc6d873f0bdfc0f5c478750ca86f3e8a1e9
Yulia <darkstarscream123@gmail.com>
2023-08-24 15:42:48 +0300

    Creation

---
+++ Song_cataloging.py
@@ -0,0 +1,67 @@
+from clustimage import Clustimage
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+import shutil
+
+def repertoire(home, img):
+    os.chdir(home)
+    # Прописываем названия будущих каталогов и создаём папки в основной рабочей директории
+
+    '''Директория, куда будут сохранены звуковые файлы каталога 
+    (при наличии этих файлов в рабочей директории)'''
+
+    catalog_sound = home+'\\'+'catalog_sound' 
+    if catalog_sound not in os.listdir(home):
+        os.mkdir(catalog_sound)
+
+    catalog_img = home+'\\'+'catalog_img' # Директория, куда будут сохранены картинки каталога
+    
+    if catalog_img not in os.listdir(home):
+        os.mkdir(catalog_img)
+        
+    os.mkdir(catalog_img)
+
+    os.chdir(img) # Меняем рабочую директорию на ту, где лежат искомые картинки
+    
+    # Основной рабочий скрипт, классифицирующий картинки
+
+    cl = Clustimage(method='hog', params_hog={'orientations':1, 'pixels_per_cell':(40,30), 'cells_per_block':(1,1)}, 
+                    verbose=None)
+
+    X = cl.import_data(img)
+    # Fit and transform
+    results = cl.fit_transform(X)
+
+    # Создаём словарь с лейблами
+
+    song_types = {}
+    for i in range(len(results['pathnames'])):
+        name = results['pathnames'][i].replace(img+'\\', '').replace('png', 'wav')
+        song_types[name] = results['labels'][i]  
+        
+    # Создаём датафрейм с лейблами и сохраняем его в csv
+
+    df = pd.DataFrame(list(song_types.items()), columns = ['Selection', 'Song type'])
+    df.to_csv(home+'\\'+'song_types.csv')
+    
+    
+    # Создаём лейблы для уникальных картинок и копируем их в каталоги с изображениями и звуками
+
+    repertoire = {}
+    for file in cl.unique()['pathnames']:
+        name = file.replace(img+'\\', '').replace('png', 'wav')
+        repertoire[name] = song_types[name]
+
+    for key, value in repertoire.items():
+        shutil.copyfile(f'{home}\\{key}', f"{catalog_sound}\\{value}.wav")
+
+    images = {}
+    for file in cl.unique()['pathnames']:
+        name = file.replace(img+'\\', '')
+        images[name] = song_types[name.replace('png', 'wav')]
+
+    for key, value in images.items():
+        shutil.copyfile(f'{img}\\{key}', f'{catalog_img}\\{value}.png')
+    os.chdir(home)    
+    return df, cl
---
+++ Visualize.py
@@ -0,0 +1,26 @@
+import librosa
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualize(home):
+    os.chdir(home)
+    img = home+'\\'+'img' # записываем имя для папки с картинками и следом создаём её
+    
+    if img not in os.listdir(home):
+        os.mkdir(img)
+        
+    sound_files = [i for i in os.listdir(home) if 'wav' in i]   
+    # Ищем файлы wav в директории.
+    
+    for sound in sound_files:
+        name = sound.replace('.wav', '') #собираем имя файла
+
+        y, sr = librosa.load(sound, sr=44100)
+        fig = plt.plot()
+        D = librosa.amplitude_to_db(np.abs(librosa.stft(y)), ref=np.max)
+        png = librosa.display.specshow(D, hop_length=512, y_axis='linear', x_axis='time',
+                                       sr=sr, cmap='Greys')
+        plt.savefig(f'{img}\\{name}.png') # сохраняем файл в папке с картинками
+    return img
